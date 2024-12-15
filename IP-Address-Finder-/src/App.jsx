ab231212c9e903a82439d07e3aa5724f39717813
sammy wlodawski <samwlodawski@gmail.com>
2024-12-15 11:42:45 -0500

    Changed a couple file name and created map component.

--- IP-Address-Finder-/src/App.jsx
+++ IP-Address-Finder-/src/App.jsx
@@ -1,35 +1,42 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from 'react';
+import ReactMapGL, { Marker } from 'react-map-gl';
+import { RiUserLocationFill } from 'react-icons';
 
-function App() {
-  const [count, setCount] = useState(0)
+const API_KEY = `<pk.eyJ1Ijoic3dsb2Rhd3NraSIsImEiOiJjbTRvejJ4eXgwb2VhMmpvcnlycm8yaHMwIn0.7pJlWj3CWosKwRedbetF-A>`;
+
+const Map = ({ lat, lon }) => {
+  const [viewport, setViewport ] = useState({
+    latitude: lat,
+    longitude: lon,
+    zoom: 14,
+    bearing: 0,
+    pitch: 0,
+    width: '100%',
+    height: '100%'
+  });
+
+  useEffect(() => {
+    const a = { ...viewport };
+    a.latitude = lat;
+    a.longitude = lon;
+    setViewport(a);
+  }, [lat, lon]);
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <div className='map'>
+      <ReactMapGL 
+      mapboxAccessToken={API_KEY} {...viewport}
+      onViewPortChange={(viewport) => setViewport(viewport)}
+      mapStyle='mapbox://styles/mapbox/streets-v11'>
+        <Marker latitude={lat} longitude={lon}>
+          <div className='mark'>
+            <RiUserLocationFill size="25px" color="blue" />
+          </div>
+        </Marker>
+      </ReactMapGL>
+      
+    </div>
+  );
+};
 
-export default App
+export default Map;
